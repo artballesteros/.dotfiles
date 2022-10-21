4a5a36b610906e3e5df678da8d361c0f79b1f7c4
artballesteros <58927442+artballesteros@users.noreply.github.com>
2022-10-20 23:49:54 -0700

    toggle full screen on startup

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,22 +1,20 @@
 local wezterm = require 'wezterm'
-local mux = wezterm.mux
 
--- To have the window toogle fullscreen at startup
+-- Toogles fullscreen window at startup
+local mux = wezterm.mux
 local fullscreen = function()
     local tab, pane, window = mux.spawn_window{}
     window:gui_window():toggle_fullscreen()
 end
-
 wezterm.on("gui-startup", fullscreen) 
-
 
 return {
     color_scheme = 'Catppuccin Mocha',
+    --color_scheme = 'Catppuccin Macchiato',
     --color_scheme = "Cyberdyne",
     --color_scheme = "cyberpunk",
     --color_scheme = "Dracula",
     --color_scheme = "Elementary",
     --color_scheme = "Elio (Gogh)",
     font = wezterm.font('Fira Code', { weight = 'Medium'}),
-    --window_decorations = "RESIZE",
 }
